a2ff5839e4a81917962381f5185941395d7ea58a
Joe <shengzhe.zhou@gmail.com>
2025-09-01 00:11:15 +1000

    feat: add Notification component with title and flexibe styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,6 +8,7 @@ import{heroIcons} from '../constants'
 import { ScrollParallax } from 'react-just-parallax'
 import { useRef } from 'react'
 import Generating from './Generating'
+import Notification from './Notification'
 
 const Hero = () => {
     const parallaxRef = useRef(null)
@@ -31,7 +32,7 @@ const Hero = () => {
                             <div className='h-[1.4rem] bg-n-10 rounded-t-[0.9rem]'/>
                             <div className='aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]'>
                                 <img src={robot} className='w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]' width={1240} height={490} alt='AI' />
-                                <Generating/>
+                                <Generating className='absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2'/>
                                 <ScrollParallax isAbsolutelyPositioned>
                                     <ul className='hidden absolute -left-[5.5rem] bottom-[7.5rem] py-1 px-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex '>
                                         {heroIcons.map((icon, index) => (
@@ -40,6 +41,12 @@ const Hero = () => {
                                             </li>
                                         ))}
                                     </ul>
+                                </ScrollParallax>
+
+                                <ScrollParallax isAbsolutelyPositioned>
+                                    <Notification
+                                      className='hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex' title='Code generation'                                    
+                                    />
                                 </ScrollParallax>
                             </div>
 
