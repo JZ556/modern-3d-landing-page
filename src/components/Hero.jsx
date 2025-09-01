5becb14defbed43d22ba02d582b1371506bf4b0d
Joe <shengzhe.zhou@gmail.com>
2025-09-01 12:09:53 +1000

    feat: complete hero section and navigation system

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,6 +9,7 @@ import { ScrollParallax } from 'react-just-parallax'
 import { useRef } from 'react'
 import Generating from './Generating'
 import Notification from './Notification'
+import CompanyLogos from './CompanyLogos'
 
 const Hero = () => {
     const parallaxRef = useRef(null)
@@ -32,7 +33,10 @@ const Hero = () => {
                             <div className='h-[1.4rem] bg-n-10 rounded-t-[0.9rem]'/>
                             <div className='aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]'>
                                 <img src={robot} className='w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]' width={1240} height={490} alt='AI' />
-                                <Generating className='absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2'/>
+                                <ScrollParallax isAbsolutelyPositioned>
+                                    <Generating className='absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2'/>
+                                </ScrollParallax>
+                                
                                 <ScrollParallax isAbsolutelyPositioned>
                                     <ul className='hidden absolute -left-[5.5rem] bottom-[7.5rem] py-1 px-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex '>
                                         {heroIcons.map((icon, index) => (
@@ -60,8 +64,10 @@ const Hero = () => {
                     <div className='absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[136%] lg:-top-[104%] -z-10'>
                         <img src={heroBackground} className=' w-full ' width={1440} height={1800} alt="hero" />
                     </div>
+
                 </div>
             </div>
+            <CompanyLogos className='hidden relative z-10 mt-20 lg:block'/>
         </div>
         <BottomLine/>    
     </Section>
