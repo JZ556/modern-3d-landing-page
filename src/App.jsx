131f9af0318b7bc8f0912a37b10d58d781e7e6f6
Joe <shengzhe.zhou@gmail.com>
2025-09-03 10:42:31 +1000

    complete and add service section

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import ButtonGradient from './assets/svg/ButtonGradient';
 import Button from './components/Button';
-import Header from './components/Header'; 
+import Header from './components/Header';
 import Hero from './components/Hero';
 import Benefit from './components/Benefit';
 import Collaboration from './components/Collaboration';
-
+import Services from './components/Services';
 const App = () => {
 
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        
-       <Header/>
-       <Hero/>
-       <Benefit/>
-       <Collaboration/>
+
+        <Header />
+        <Hero />
+        <Benefit />
+        <Collaboration />
+        <Services />
       </div>
-      <ButtonGradient/>
+      <ButtonGradient />
     </>
-    
+
   )
 }
 
--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import loading from '../assets/loading.png'
+import React from 'react';
+import loading from '../assets/loading.png';
 
-const Generating = ({className}) => {
+const Generating = ({ className }) => {
   return (
     <div className={`flex items-center h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] ${className || ''} text-base`}>
-        <img className='w-5 h-5 mr-4 ' src={loading} alt="loading" />
-        AI is generating...
+      <img className='w-5 h-5 mr-4 ' src={loading} alt="loading" />
+      AI is generating...
     </div>
   )
 }
